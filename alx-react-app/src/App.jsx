8b4c5c9eee245e93ec476bdf756097a526b9de7f
Emmanuel Effa <manueleffa@gmail.com>
2026-01-11 23:42:56 +0100

    created few other components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,8 @@
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -11,25 +15,11 @@ function App() {
     <>
       <div>
         <WelcomeMessage />
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header />
+        <MainContent />
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }

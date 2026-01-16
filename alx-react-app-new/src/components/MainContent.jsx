0eefed27bb7b3b87a43639641db8c1429c34cf04
Emmanuel Effa <manueleffa@gmail.com>
2026-01-16 21:32:29 +0100

    corrected the main.jsx component to pass the checker

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
   return (
     <main style={{textAlign: 'center', fontSize: '20px'}}>
-      <p>I love to visit <span style={{ fontWeight: 'bold', color: 'red' }}>New York</span>, <span style={{ fontWeight: 'bold', color: 'red' }}>Paris</span>, and <span style={{ fontWeight: 'bold', color: 'red' }}>Tokyo</span>.</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }

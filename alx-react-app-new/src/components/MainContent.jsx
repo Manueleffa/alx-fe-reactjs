ce5df1022953f8a657394fc1645e656e66aa9a40
Emmanuel Effa <manueleffa@gmail.com>
2026-01-16 21:29:01 +0100

    duplicated the app directory and added inline styling to the components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{textAlign: 'center', fontSize: '20px'}}>
+      <p>I love to visit <span style={{ fontWeight: 'bold', color: 'red' }}>New York</span>, <span style={{ fontWeight: 'bold', color: 'red' }}>Paris</span>, and <span style={{ fontWeight: 'bold', color: 'red' }}>Tokyo</span>.</p>
+    </main>
+  );
+}
+
+export default MainContent;
